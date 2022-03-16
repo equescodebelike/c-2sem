781c646031c37b6b791461a5dd9106ccb62aa809
equescodebelike <nep3600@gmail.com>
2022-03-16 13:07:03 +0300

    preparing task2

---
+++ task2/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+double factorial(int f) {
+    if (f < 0) {
+        return 0;
+    }
+    if (f == 0) {
+        return 1;
+    } else {
+        return f * factorial(f - 1);
+    }
+}
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Введите значение n: \n";
+    int n;
+    cin >> n;
+    double result = 0;
+    for (int i = 1; i <= n; ++i) {
+        result += i / factorial(i + 1);
+    }
+    printf("Ответ: %.40lf", result);
+
+    cin.ignore();
+    return 0;
+}
