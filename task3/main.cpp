206e0f943aa24318bddc3287bc8c635180b23c19
equescodebelike <nep3600@gmail.com>
2022-03-28 20:29:26 +0300

    editing task 3

---
+++ task3/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int len;
+    cout << "Arr length: \n";
+    cin >> len;
+    char arr[len];
+    cout << "Arr elements: \n";
+    for (int i = 0; i < len; ++i) {
+        cin >> *(arr + i);
+    }
+
+    /* for (int i = 0, c = 1; i < len; ++i, c++) {
+        if (c % 3 == 0) {
+            cout << *(arr + i);
+        }
+    } */
+
+    for (int i = 2; i < len; i += 3) {
+        cout << *(arr + i);
+    }
+
+    cin.ignore();
+    return 0;
+}
